ed97594749aae9377bce60b992a227e309967d69
MohanaPoojary <mohan9980548223@gmail.com>
2024-11-20 15:15:01 +0530

    opening chrome browser

---
+++ chrome_browser_launch.py
@@ -0,0 +1,13 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser=p.chromium.launch(headless=False)
+    page=browser.new_page()
+    page.goto('https://demo.automationtesting.in/Index.html')
+    print('Chrome Successfully Opened')
+    print(page.title())
+    page.wait_for_timeout(3000)
+    
+
+
+    
